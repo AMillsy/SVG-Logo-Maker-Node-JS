43f8d5ac716cc788be943454ce355c4460c622c0
AMillsy <00adammills00@gmail.com>
2023-06-15 17:07:31 +0100

    adding more tests to the SVG

--- test/SVG.test.js
+++ test/SVG.test.js
@@ -3,10 +3,24 @@ const { SVGText } = require("../lib/SVGText");
 const { Square } = require("../lib/shapes");
 
 describe("SVG", () => {
-  it("Should create an SVG Element that is a blue square with text saying Dad that is white", () => {
+  it("Should create an SVG Element that is a blue square with text saying DAD that is white", () => {
     const shapeColour = "blue";
     const text = "DAD";
     const textColour = "white";
+
+    const svgShape = new Square(shapeColour).render();
+    const svgText = new SVGText(text, textColour).render();
+    const expectResult = `<svg width="300" height="200">
+    ${svgShape}
+    ${svgText}
+    </svg>`;
+
+    expect(new SVG(svgShape, svgText).renderSVG()).toEqual(expectResult);
+  });
+  it("Should create an SVG Element that is a orange square with text saying JOB that is blue", () => {
+    const shapeColour = "orange";
+    const text = "JOB";
+    const textColour = "blue";
 
     const svgShape = new Square(shapeColour).render();
     const svgText = new SVGText(text, textColour).render();
